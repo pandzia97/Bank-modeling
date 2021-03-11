3805e5e12f7dfe4167d63fa65be85072f79cf2eb
pandzia97 <andzia.okarmus@gmial.com>
2021-03-11 12:06:00 +0100

    Create methods for switch/case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,55 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void createBank(final Scanner scanner, final List<Bank> banks) {
+        System.out.println("Create new bank");
+        final String nameBank = scanner.nextLine();
+        System.out.println("Enter the address");
+        System.out.println("City:");
+        final String cityNewBank = scanner.next();
+        System.out.println("Zip code:");
+        final int zipCodeNewBank = scanner.nextInt();
+        System.out.println("Street:");
+        final String streetNewBank = scanner.nextLine();
+        System.out.println("House number:");
+        final int houseNumberNewBank = scanner.nextInt();
+        System.out.println("Apartment number:");
+        final int apartmentNumberNewBank = scanner.nextInt();
+        final Address address = new Address(cityNewBank, zipCodeNewBank, streetNewBank, houseNumberNewBank, apartmentNumberNewBank);
+        final BankBranch bankBranch = new BankBranch(address);
+        final Bank bank = new Bank(nameBank, bankBranch);
 
-        List<Bank> banks = new ArrayList<>();
+        banks.add(bank);
+    }
+
+    public static void listBank(final List<Bank> banks) {
+        System.out.println("List banks");
+        for (int i = 0; i < banks.size(); i++) {
+            System.out.println(i);
+            System.out.println(banks.get(i));
+        }
+    }
+
+    public static void selectBank(final Scanner scanner, final List<Bank> banks) {
+        System.out.println("Select bank");
+        final int select = scanner.nextInt();
+        final Bank bankSelect = banks.get(select);
+        System.out.println(bankSelect);
+    }
+
+    public static void deleteBank(final Scanner scanner, final List<Bank> banks) {
+        System.out.println("Delete bank");
+        final int remove = scanner.nextInt();
+        if (remove < banks.size()) {
+            banks.remove(remove);
+        } else {
+            System.out.println("Wrong id");
+        }
+    }
+
+    public static void main(final String[] args) {
+
+        final List<Bank> banks = new ArrayList<>();
         while (true) {
             System.out.println("What do you want to do?");
             System.out.println("1. Create new bank");
@@ -19,51 +65,22 @@ public class Main {
 
             final Scanner scanner = new Scanner(System.in);
 
-            int choice = scanner.nextInt();
+            final int choice = scanner.nextInt();
 
             switch (choice) {
                 case 0:
                     return;
                 case 1:
-                    System.out.println("Create new bank");
-                    String nameBank = scanner.nextLine();
-                    System.out.println("Enter the address");
-                    System.out.println("City:");
-                    String cityNewBank = scanner.next();
-                    System.out.println("Zip code:");
-                    int zipCodeNewBank = scanner.nextInt();
-                    System.out.println("Street:");
-                    String streetNewBank = scanner.nextLine();
-                    System.out.println("House number:");
-                    int houseNumberNewBank = scanner.nextInt();
-                    System.out.println("Apartment number:");
-                    int apartmentNumberNewBank = scanner.nextInt();
-                    Address address = new Address(cityNewBank, zipCodeNewBank, streetNewBank, houseNumberNewBank, apartmentNumberNewBank);
-                    BankBranch bankBranch = new BankBranch(address);
-                    Bank bank = new Bank(nameBank, bankBranch);
-
-                    banks.add(bank);
+                    createBank(scanner, banks);
                     break;
                 case 2:
-                    System.out.println("List banks");
-                    for (int i = 0; i < banks.size(); i++) {
-                        System.out.println(i);
-                        System.out.println(banks.get(i));
-                    }
+                    listBank(banks);
                     break;
                 case 3:
-                    System.out.println("Select bank");
-                    int select = scanner.nextInt();
-                    Bank selectBank = banks.get(select);
+                    selectBank(scanner, banks);
                     break;
                 case 4:
-                    System.out.println("Delete bank");
-                    int remove = scanner.nextInt();
-                    if (remove < banks.size()) {
-                        banks.remove(remove);
-                    } else {
-                        System.out.println("Wrong id");
-                    }
+                    deleteBank(scanner, banks);
                     break;
                 default:
                     System.out.println("Wrong choice");
